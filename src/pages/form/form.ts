3b865a78549a54c290cc049dc03a4c0368651d2c
Prosenjit Manna <developer.prosenjit@gmail.com>
2017-01-13 23:06:01 +0530

    form submit with reactive model

--- src/pages/form/form.ts
+++ src/pages/form/form.ts
@@ -1,16 +1,32 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'page-form',
   templateUrl: 'form.html'
 })
 export class FormPage {
+  loginform : FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(
+   public navCtrl: NavController,
+   public navParams: NavParams,
+   fb: FormBuilder
+   ) {
+     this.loginform = fb.group({
+       email: null,
+       password: null
+     })
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad FormPage');
+  }
+
+  loginSubmit(form) {
+    console.log('Reactive form data');
+    console.log(form._value);
   }
 
 }
